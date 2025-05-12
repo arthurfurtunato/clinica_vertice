c0051514b61da661f61be9d05df64c95c3303a5f
Arthur Furtunato <arthurfurtunato@MacBook-Pro-de-Arthur.local>
2025-05-12 09:47:23 -0300

    navbar e introdução

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
